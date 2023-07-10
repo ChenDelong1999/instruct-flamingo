743b178449b3dda970217977fc53ff79609837e9
chendelong1999 <673939938@qq.com>
2023-07-10 18:28:33 +0800

    enabling import v1 and v2

--- open_flamingo/__init__.py
+++ open_flamingo/__init__.py
@@ -1,2 +1,4 @@
 from .src.flamingo import Flamingo
 from .src.factory import create_model_and_transforms
+from .src_v1.factory import create_model_and_transforms as create_model_and_transforms_v1
+from .instruction_tuning.train_utils import prepare_model_for_tuning, get_params_count_summary, resume_from_checkpoints
