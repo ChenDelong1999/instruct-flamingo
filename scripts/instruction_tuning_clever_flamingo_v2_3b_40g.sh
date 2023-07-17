bcde73ff9b264b189cd831f66d81edc2401169ca
chendelong1999 <673939938@qq.com>
2023-07-17 18:14:16 +0800

    update train/valscripts

--- scripts/instruction_tuning_clever_flamingo_v2_3b_40g.sh
+++ scripts/instruction_tuning_clever_flamingo_v2_3b_40g.sh
@@ -11,7 +11,7 @@ export PYTHONPATH="$PYTHONPATH:open_flamingo"
 CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' torchrun --nnodes=1 --nproc_per_node=8 --master_port=29502 open_flamingo/instruction_tuning/train.py \
     --instruction_data 'instruction_dataset/configs/datasets.json' \
     --instruction_prompt_templete 'guanaco-no-prompt' \
-    --run_name 'runs/0712-clever_flamingo_v2_3b-2k_context-40G' \
+    --run_name 'runs/0717-clever_flamingo_v2_3b-2k_context-40G-resume-from-0716' \
     --seed 42 \
     --vision_encoder_path 'ViT-L-14-336' \
     --lm_path '/cpfs/user/chendelong/cache/mpt-1b-redpajama-200b-dolly' \
@@ -19,18 +19,19 @@ CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' torchrun --nnodes=1 --nproc_per_node=8 --
     --cross_attn_every_n_layers 1 \
     --freeze_lm_embeddings \
     --tuning_config '/cpfs/user/chendelong/open_flamingo_v2/open_flamingo/instruction_tuning/tuning_config/lora[lm+xqttn]+perceiver.json' \
-    --resume_from_checkpoint '/cpfs/user/chendelong/cache/OpenFlamingo-3B-vitl-mpt1b-langinstruct/checkpoint.pt' \
+    --resume_from_checkpoint '/cpfs/user/chendelong/cache/OpenFlamingo-3B-vitl-mpt1b-langinstruct/checkpoint.pt,/cpfs/user/chendelong/open_flamingo_v2/runs/0716-clever_flamingo_v2_3b-2k_context-40G/checkpoint_4.pt' \
+    --continue_training \
     --max_length 2048 \
     --multiturn_augmentation 32 \
     --max_img 16 \
     --skip_check_overlength \
-    --train_num_samples 5000000 \
+    --train_num_samples 10000000 \
     --epoch_num_samples 500000 \
     --batch_size 2 \
     --learning_rate 1e-4 \
     --gradient_accumulation_steps 4 \
     --precision 'bf16' \
-    --workers 32 \
+    --workers 8 \
     --num_epochs 20 \
     --lr_scheduler constant \
     --warmup_steps 1000 \
--- scripts/run_eval_instruction.sh
+++ scripts/run_eval_instruction.sh
@@ -6,12 +6,12 @@ eval "$(conda shell.bash hook)"
 conda activate instruct_flamingo
 which python
 
-CUDA_VISIBLE_DEVICES='6' python open_flamingo/instruction_tuning/instruction_dataset_inference.py \
+CUDA_VISIBLE_DEVICES='1' python open_flamingo/instruction_tuning/instruction_dataset_inference.py \
     --lm_path "/cpfs/user/chendelong/cache/mpt-7b" \
     --vision_encoder_path "ViT-L-14-336" \
     --vision_encoder_pretrained "openai" \
     --tuning_config 'open_flamingo/instruction_tuning/tuning_config/lora[lm+xqttn]+perceiver.json' \
-    --checkpoint_paths '/cpfs/user/chendelong/cache/OpenFlamingo-9B-vitl-mpt7b.pt,/cpfs/user/chendelong/open_flamingo_v2/runs/0709-clever_flamingo_v2-8x80g-2k_context/checkpoint_20.pt'  \
+    --checkpoint_paths '/cpfs/user/chendelong/cache/OpenFlamingo-9B-vitl-mpt7b.pt,/cpfs/user/chendelong/open_flamingo_v2/runs/0713-clever_flamingo_v2_9b-2k_context-80G/checkpoint_4.pt'  \
     --cross_attn_every_n_layers 4 \
     --instruction_path '/cpfs/user/chendelong/open_flamingo/datasets/validation_selected_for_v2.json' \
     --instruction_prompt_templete 'guanaco-no-prompt' \
